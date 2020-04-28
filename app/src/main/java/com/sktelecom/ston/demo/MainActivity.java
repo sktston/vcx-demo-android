e05ddfc3582391a900ed3bc37767bd97ae305e17
Thomas Kim <jeong.kim@sk.com>
2020-04-28 19:24:05 +0900

    Set up the environment and init LibVcx if it is not initialized.

--- app/src/main/java/com/sktelecom/ston/demo/MainActivity.java
+++ app/src/main/java/com/sktelecom/ston/demo/MainActivity.java
@@ -3,6 +3,10 @@ package com.sktelecom.ston.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.system.ErrnoException;
+import android.system.Os;
+
+import com.evernym.sdk.vcx.LibVcx;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            Os.setenv("EXTERNAL_STORAGE", getExternalFilesDir(null).getAbsolutePath(), true);
+        } catch (ErrnoException e) {
+            e.printStackTrace();
+        }
+
+        if (!LibVcx.isInitialized()) {
+            LibVcx.init();
+        }
     }
 }
